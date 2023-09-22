654b811a3c503e34df50d8ada836d0dff49dc88d
svn <ipsvn@protonmail.com>
2023-09-22 01:22:01 +0100

    well this is embarassing

--- src/main/java/lol/svn/reward/impl/HypixelRewardImpl.java
+++ src/main/java/lol/svn/reward/impl/HypixelRewardImpl.java
@@ -136,7 +136,7 @@ public class HypixelRewardImpl implements HypixelReward {
                 throw new RuntimeException(e);
             }
 
-        });
+        }, executor);
     }
 
     @Override
